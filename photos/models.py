06f1c60b446f79ba9239afb0db4cebacaea7b06d
CosBett <cosmasbett9@gmail.com>
2022-02-26 08:34:13 +0300

    update location model methods

--- photos/models.py
+++ photos/models.py
@@ -1,7 +1,21 @@
+from multiprocessing import set_forkserver_preload
 from django.db import models
 
 class Location(models.Model):
   name = models.CharField(max_lenght= 50)
 
+  def save_location(self):
+    self.save()
+
+  def update_location(self, name):
+    self.name = name
+    self.save()
+
+  def delete_location(self):
+    self.delete()  
+
 class Category(models.Model):
+  name = models.CharField(max_lenght= 50)
+
+class Image(models.Model):
   name = models.CharField(max_lenght= 50)
