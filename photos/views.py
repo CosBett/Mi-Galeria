c615e1c9068801ab3d865f19a42811ffae332a9a
CosBett <cosmasbett9@gmail.com>
2022-02-26 03:37:28 +0300

    render homepage

--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# Homepage view function 
+def index(request):
+  homepage = {}
+  return render (request, 'all-photos/index.html', homepage)
