53fd1e7528938d32a3b8962973a5a8fb0b52ad3c
CosBett <cosmasbett9@gmail.com>
2022-03-01 18:03:16 +0300

    add procfile file and update it

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn mi-galeria.wsgi --log-file -
--- gallery/settings.py
+++ gallery/settings.py
@@ -128,9 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-    ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Default primary key field type
@@ -140,3 +138,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+
