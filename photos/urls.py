30432bb9a7ed041f7542a6398b27ef1752f4cd40
CosBett <cosmasbett9@gmail.com>
2022-03-01 16:39:16 +0300

    fix page responsiveness

--- photos/urls.py
+++ photos/urls.py
@@ -6,6 +6,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.index, name ='homepage'),
     path('location/<location>/', views.locationImg_results, name= 'location'),
+    path('category/<category>/', views.categoryImg_results, name= 'category'),
+
     path('search/', views.search_results, name = 'search_results'),
 
     ]
