41941747db0f64f1f635cfdd203689d9b2de90b6
CosBett <cosmasbett9@gmail.com>
2022-03-01 10:33:47 +0300

    update modal target id to display images

--- photos/views.py
+++ photos/views.py
@@ -12,7 +12,10 @@ def index(request):
 
 def locationImg_results(request,location):
     images = Image.filter_by_location(location)
-    location = {'images':images}
+    all_locations = Location.objects.all()
+    all_categories = Category.objects.all()
+    location = {'images':images,'all_locations':all_locations, 'all_categories':all_categories }
+
     return render(request, 'all-photos/location.html',location)
 # def nav_items(request):
 #     all_locations = Location.objects.all()
@@ -22,9 +25,11 @@ def search_results(request):
     if 'searchImg' in request.GET and request.GET['searchImg']:
         category = request.GET.get('searchImg')
         searched_images =Image.search_by_category(category)
+        all_locations = Location.objects.all()
+        all_categories = Category.objects.all()
         message = f'{category}'
-        category_images = {'images':searched_images, 'message': message}
-        return render(request, ' all-photos/search.html', category_images)
+        category_images = {'all_locations':all_locations, 'all_categories':all_categories, 'images':searched_images, 'message': message}
+        return render(request, 'all-photos/search.html', category_images)
     else:
         message = 'Please type category to search'
         return render(request, 'all-photos/search.html', {'message':message})
