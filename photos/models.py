bbc9359c8ff6c4a27b3edaf44c94fad512e25480
CosBett <cosmasbett9@gmail.com>
2022-03-01 06:02:20 +0300

    updateImage  models class methods

--- photos/models.py
+++ photos/models.py
@@ -31,29 +31,15 @@ class Category(models.Model):
     
 class Image(models.Model):
   image = models.ImageField(upload_to='pictures',blank=True)
-  name = models.CharField(max_length=60)
-  description = models.CharField(max_length=1500)
+  name = models.CharField(max_length=80)
+  description = models.TextField(max_length=1500)
   location = models.ForeignKey(Location,on_delete=models.CASCADE)
   category = models.ForeignKey(Category,on_delete=models.CASCADE)
   
-  def save_image(self):
-    self.save()
-
-  def update_image(self, name,image, description,location,category):
-    self.name = name
-    self.image = image
-    self.description = description
-    self.location = location
-    self.category = category
-
-    self.save()
-
-  def delete_image(self):
-    self.delete() 
-  
+ 
   @classmethod
-  def get_image_by_id(cls,imageId):
-    images_results = cls.objects.get(id = imageId)
+  def get_image_by_id(cls,id):
+    images_results = cls.objects.filter(id = id).all()
     return images_results
 
   @classmethod
@@ -63,8 +49,37 @@ class Image(models.Model):
   
   @classmethod
   def filter_by_location(cls, location):
-    locationImg_results = cls.objects.filter(location__name= location)
+    locationImg_results = cls.objects.filter(location__name= location).all()
     return locationImg_results
 
+  @classmethod
+  def filter_by_category(cls, category):
+    categoryImg_results = cls.objects.filter(category__name= category).all()
+    return categoryImg_results
+
+  @classmethod
+  def search_by_category(cls, category):
+    images = cls.objects.filter(category__name__icontains=category)
+    return images
+  
   def __str__(self):
       return self.name
+
+  def save_image(self):
+    self.save()
+
+
+  def delete_image(self):
+    self.delete() 
+  
+  def update_image(self, name,image, description,location,category):
+    self.name = name
+    self.image = image
+    self.description = description
+    self.location = location
+    self.category = category
+
+    self.save()
+
+ 
+  
