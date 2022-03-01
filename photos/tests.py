3edf2e65d4069849fa9b8d845d4f97cf951dd52e
CosBett <cosmasbett9@gmail.com>
2022-03-01 17:14:24 +0300

    create test for Image  model class

--- photos/tests.py
+++ photos/tests.py
@@ -46,5 +46,58 @@ class Test_Category(TestCase):
 
     def test_delete_category(self):
         self.location.delete_category()
-        location = Category.objects.all()
+        category = Category.objects.all()
         self.assertTrue(len(category) == 0)        
+
+class Test_Image(TestCase):
+    def setUp(self):
+        self.location = Location(name='Moyale')
+        self.location.save_location()
+        self.category = Category(name='Nature')
+        self.category.save_category()
+        self.image_test = Image(id=1, name='image', description='this is a test image', location=self.location,
+                                category=self.category)   
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image_test, Image))
+
+    def test_save_image(self):
+        self.image_test.save_image()
+        after = Image.objects.all()
+        self.assertTrue(len(after) > 0)
+
+    def test_delete_image(self):
+        self.image_test.delete_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images) == 0)
+
+    def test_update_image(self):
+        self.image_test.save_image()
+        self.image_test.update_image(self.image_test.id, 'pictures/test.png')
+        changed_img = Image.objects.filter(image='pictures/test.png')
+        self.assertTrue(len(changed_img) > 0)
+
+    def test_get_image_by_id(self):
+        found_image = self.image_test.get_image_by_id(self.image_test.id)
+        image = Image.objects.filter(id=self.image_test.id)
+        self.assertTrue(found_image, image) 
+
+    def test_search_image_by_category(self):
+        category = 'Nature'
+        found_img = self.image_test.search_by_category(category)
+        self.assertTrue(len(found_img) > 1)
+
+    def test_filter_image_by_location(self):
+        self.image_test.save_image()
+        found_images = self.image_test.filter_by_location(location='Moyale')
+        self.assertTrue(len(found_images) == 1) 
+
+    def test_filter_image_by_category(self):
+        self.image_test.save_image()
+        found_images = self.image_test.filter_by_category(category='Nature')
+        self.assertTrue(len(found_images) == 1)        
+
+    def tearDown(self):
+        Image.objects.all().delete()
+        Location.objects.all().delete()
+        Category.objects.all().delete()    
